00b4320fc3c648b3b0668d4a91285f855bead384
Xinzi He <51917510+Novestars@users.noreply.github.com>
2021-04-06 13:09:13 -0400

    Update uflow_net.py

--- uflow_net.py
+++ uflow_net.py
@@ -1,23 +1,17 @@
-import uflow_model
-import uflow_utils
+import RRN_model
+import RRN_utils
 import torch.nn as nn
 
-class UFlow(nn.Module):
+class RRN(nn.Module):
     def __init__(self, num_channels=3, num_levels = 5, use_cost_volume=True, action_channels = 0):
         super(UFlow, self).__init__()
 
-        self._pyramid = uflow_model.PWCFeaturePyramid(num_levels=num_levels, num_channels=num_channels)
-        self._flow_model = uflow_model.PWCFlow(num_levels = num_levels, num_channels_upsampled_context=32,
+        self._pyramid = RRN_model.RRNFeaturePyramid(num_levels=num_levels, num_channels=num_channels)
+        self._flow_model = RRN_model.RRNFlow(num_levels = num_levels, num_channels_upsampled_context=32,
                                                use_cost_volume=use_cost_volume, use_feature_warp=True,
                                                action_channels=action_channels)
 
     def forward(self, img1, img2, actions=None):
-        """Compute the flow between image pairs
-        :param img1: [BCHW] image 1 batch
-        :param img2: [BCHW] image 2 batch
-        :param actions: None or [BC] self-action for each pair, C == self.action_channels
-        :return: [B2HW] flow tensor for each image pair in batch, feature pyramid list for img pairs
-        """
         fp1 = self._pyramid(img1)
         fp2 = self._pyramid(img2)
         flow = self._flow_model(fp1, fp2, actions)
